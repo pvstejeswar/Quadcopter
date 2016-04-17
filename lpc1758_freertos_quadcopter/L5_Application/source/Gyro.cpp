f1da588046230d8417e789c4b247e1749928e34a
Pvs Tejeswar <pvstejeswar.gmail.com>
2016-04-17 13:52:26 -0700

    Small Changes in Gyro Code

--- lpc1758_freertos_quadcopter/L5_Application/source/Gyro.cpp
+++ lpc1758_freertos_quadcopter/L5_Application/source/Gyro.cpp
@@ -13,7 +13,7 @@ scheduler_task("pwm", 3 * 512, priority), taskRateHz(rateHz)
 {
     qh = xQueueCreate(1, 20);
     addSharedObject(gyroData,qh);
-    setRunDuration(taskRateHz);
+    //setRunDuration(taskRateHz);
 }
 
 bool GyroTask::run(void*p )
@@ -21,6 +21,13 @@ bool GyroTask::run(void*p )
     printf("AS_X----------: %d\n", IMU.AS_getX());
     printf("AS_Y----------: %d\n", IMU.AS_getY());
     printf("AS_Z----------: %d\n", IMU.AS_getZ());
+
+    printf("GS_X----------: %d\n", IMU.GS_getX());
+    printf("GS_Y----------: %d\n", IMU.GS_getY());
+    printf("GS_Z----------: %d\n", IMU.GS_getZ());
+
+    delay_ms(3000);
+
     printf("\n\n\n");
     return true;
 }
